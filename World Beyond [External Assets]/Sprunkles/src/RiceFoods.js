47605a54de09aa6fc7580b2089070a4a3e2d7769
Sprunkles137 <64864915+Sprunkles137@users.noreply.github.com>
2022-05-03 19:04:07 -0500

    Updated rice food items

--- World Beyond [External Assets]/Sprunkles/src/RiceFoods.js
+++ World Beyond [External Assets]/Sprunkles/src/RiceFoods.js
@@ -40,32 +40,40 @@ const Foods = [
 const Output = {
 	texture_data: {}
 }
-for(let food of Foods) {
-	fs.writeFileSync(`./World Beyond [Data]/items/sets/rice_food/${food.identifier}_rice_bowl.json`, JSON.stringify({
-		format_version: "1.16.100",
-		"minecraft:item": {
-			description: {
-				identifier: `worldbeyond:${food.identifier}_rice_bowl`,
-				category: "equipment"
-			},
-			components: {
-				"minecraft:food": {
-					nutrition: Math.floor(food.nutrition * 1.5),
-					saturation_modifier: food.saturation,
-					using_converts_to: "bowl"
+class RiceFoodItem {
+	constructor() {
+		this.bp_item = {}
+	}
+	write_bp(item) {
+		this.bp_item = {
+			format_version: "1.16.100",
+			"minecraft:item": {
+				description: {
+					identifier: `worldbeyond:${item.identifier}_rice_bowl`,
+					category: "equipment"
 				},
-				"minecraft:max_stack_size": 16,
-				"minecraft:use_duration": 32,
-				"minecraft:item_storage": {
-					capacity: 4
-				},
-				"minecraft:icon": {
-					texture: `${food.identifier}_rice_bowl`
+				components: {
+					"minecraft:food": {
+						nutrition: Math.floor(item.nutrition * 1.5),
+						saturation_modifier: item.saturation,
+						using_converts_to: "bowl"
+					},
+					"minecraft:max_stack_size": 16,
+					"minecraft:use_duration": 32,
+					"minecraft:item_storage": {
+						capacity: 4
+					},
+					"minecraft:icon": {
+						texture: `${item.identifier}_rice_bowl`
+					}
 				}
 			}
 		}
-	}, null, 2)
-	)
+		fs.writeFileSync(`./World Beyond [Data]/items/sets/rice_food/${item.identifier}_rice_bowl.json`, JSON.stringify(this.bp_item, null, 2))
+	}
+}
+for(let food of Foods) {
+	new RiceFoodItem().write_bp(food)
 	Output.texture_data[`${food.identifier}_rice_bowl`] = {
 		textures: `textures/items/${food.identifier}_rice_bowl`
 	}
